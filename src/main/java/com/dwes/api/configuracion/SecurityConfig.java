5e8ae2096189a10803ee3a1002a8a026f848f25c
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2024-01-14 23:58:52 +0100

    Update SecurityConfig.java

--- src/main/java/com/dwes/api/configuracion/SecurityConfig.java
+++ src/main/java/com/dwes/api/configuracion/SecurityConfig.java
@@ -10,23 +10,39 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig{
-	@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        http
-        .authorizeHttpRequests((authz) -> authz
-               // .requestMatchers(HttpMethod.GET, "/api/v1/jabones/**").permitAll() 
-                .requestMatchers("/api/v1/jabones/**").permitAll() // Permite todas las solicitudes a /api/v1/jabones/**
-                .requestMatchers("/**").permitAll() // Permite todas las solicitudes a Swagger UI
-                .anyRequest().authenticated()); // Todas las demás solicitudes necesitan autenticación
-        
+	@Configuration
+	@EnableWebSecurity
+	public class SecurityConfig{
 
-        // Si utilizas CSRF y deseas mantenerlo, podrías necesitar una configuración específica aquí
-        // Para APIs REST, comúnmente CSRF se deshabilita así:
-        http.csrf((csrf) -> csrf.disable());
-        return http.build();
-    }
+		@Bean
+	    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		
+			http
+			
+
+			.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+	        .authorizeHttpRequests((authz) -> authz
+	               // .requestMatchers(HttpMethod.GET, "/api/v1/jabones/**").permitAll() 
+	        		.requestMatchers("/api/v1/jabones/**").permitAll() // Permite todas las solicitudes a /api/v1/jabones/**
+	        		
+	        		.requestMatchers("/**").permitAll() // Permite todas las solicitudes a Swagger UI
+	                .anyRequest().authenticated()); // Todas las demás solicitudes necesitan autenticación
+	       /**
+	        * Esto significa que tu aplicación no creará sesiones de usuario.
+	        *  En lugar de eso, cada solicitud se considera independiente 
+	        *  y no se mantiene información de sesión entre ellas. 
+	        *  Esta configuración es comúnmente utilizada en aplicaciones 
+	        *  RESTful o APIs donde la autenticación se realiza en cada 
+	        *  solicitud y no se necesita mantener un estado de sesión.
+	        */
+			http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+	        // Para APIs REST, comúnmente CSRF se deshabilita así:
+           
+			http.csrf(csrf -> csrf
+                    .ignoringRequestMatchers("/h2-console/**"));
+			http.headers(headers -> headers.frameOptions().disable());
+	        
+	        return http.build();
+	    }
 
 	}
